221452d6bbf24ef25225e212dfdf5ae23ae0b8b0
João Otávio <joaotavioos@outlook.com>
2022-02-09 23:51:09 -0300

    Add reducers

---
+++ store/modules/rootReducer.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+
+import auth from "./auth/reducer";
+import search from "./admin/shared/search/reducer";
+import category from "./admin/category/reducer";
+import product from './admin/product/reducer';
+
+export default combineReducers({
+  auth,
+  search,
+  category,
+  product,
+});
