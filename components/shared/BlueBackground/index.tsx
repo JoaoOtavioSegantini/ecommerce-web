294541bf64827ef17a9125975df2df2217edd646
João Otávio <joaotavioos@outlook.com>
2022-02-09 23:56:10 -0300

    Add BlueBlackground component

---
+++ components/shared/BlueBackground/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const BlueBackground: React.FC = ({ children }) => {
+  return <div className={styles.main}>{children}</div>;
+};
+
+export default BlueBackground;
