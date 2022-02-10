a7e1844bcf9030c1b558e59dd33ba8e85b89b550
João Otávio <joaotavioos@outlook.com>
2022-02-09 23:57:40 -0300

    Add Logo

---
+++ components/shared/Logo/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./styles.module.css";
+
+const Logo: React.FC = () => {
+  return (
+    <Link href="/">
+      <a className={styles.logo}>
+        <Image
+          src="/assets/logo-games.png"
+          alt="Logo OneBitGames"
+          width={220}
+          height={40}
+        />
+      </a>
+    </Link>
+  );
+};
+
+export default Logo;
